6d666b91157740b0cf62bf519151e5d818c800f7
yelnats10 <yelnats10@gmail.com>
2017-10-10 23:23:48 -0400

    updated more styles

--- assets/javascript/foodies.js
+++ assets/javascript/foodies.js
@@ -8,8 +8,6 @@ $(document).ready(function() {
         }
     });
 
-
-
     $(window).scroll(function() {
         var wScroll = $(this).scrollTop();
 
@@ -18,4 +16,14 @@ $(document).ready(function() {
             $("footer").addClass("showing");
         }
     });
+
+
+   var $sticky = $('#sticky-div'),
+       Top = $sticky.offset().top;
+
+   $(window).scroll(function() {
+        $sticky.toggleClass('sticky', $(window).scrollTop() > Top);
+    });
+
+
 });
